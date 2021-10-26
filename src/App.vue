f6181f5e7c3b219a6aeee956969c7edab53cc86a
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-10-26 10:26:27 +0300

    feat: add files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+    <div>
+         <form action="#" method="post">  
+    
+    <div>
+      <label for="day">Day:</label>
+      <input placeholder="кол-во дней" type="text" id="day" name="user_day">  
+    </div>  
+
+    <div>
+      <label for="location">Location:</label>
+      <input placeholder="город на английском" type="location" id="location" name="user_location">  
+    </div> 
+
+    <div>
+    <button type="submit">Show the weather forecast</button>
+    </div>
+
+</form>
+    </div>
+    <!--<button @click="addLike">Like</button> IF v-on:click='addLike'-->
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+<script>
+const API_TOKEN = 'YmVmYjliYmQtN2U0MC00MzkyLTgwNjUtYTA1ZTRkMzAwZTc0';
+const API_URL = 'https://api.m3o.com/v1/weather/Forecast';
+
+export default{
+    data(){
+        return{
+            // likes:0,
+            // dislikes:0,
+        }
+    },
+    // methods:{
+    //     addLike(){
+    //         this.likes +=1;
+    //     },
+    //     addDislike(){
+    //         this.dislikes +=1;
+
+    //     }
+    // }
 }
+
+fetch(API_URL +  `?days=2&location=London`, {
+        method:"GET",
+        headers:{
+            "Content-Type": `application/json`,
+            "Authorization": `Bearer ${API_TOKEN}`,
+        }
+     })
+     .then(resp=>resp.json())
+     .then((data)=>{console.log(data)})
+     .catch((error)=>{console.log(error)})
+
+
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
